dd24181e4ebdd55161c0971efe4ee69e2580a52b
zeroproj <43253744+zeroproj@users.noreply.github.com>
2023-10-03 13:10:51 -0300

    TheHive

--- MHSys/dep.sh
+++ MHSys/dep.sh
@@ -5,16 +5,6 @@ function install_dependencies() {
     apt-get install -y openjdk-8-jre-headless
     apt install python3-pip -y
 }
-function install_elasticsearch() {
-    echo -e "- Configurando Repositorio Elastic"
-    curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/elasticsearch.gpg --import && chmod 644 /usr/share/keyrings/elasticsearch.gpg
-    echo "deb [signed-by=/usr/share/keyrings/elasticsearch.gpg] https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
-}
-function install_wserver() {
-    echo -e "- Configurando Repositorio WServer"
-    curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
-    echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-}
 function conf_java() {
     echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
     export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
@@ -37,8 +27,6 @@ else
     if [ "$P_01" == "-a" ]; then
         apt update
         install_dependencies
-        install_elasticsearch
-        install_wserver
         conf_java
         conf_cassandra
         install_thehive
